58b2be0fe0976fe6828858dc6bf0a31a089714e2
Shivam Gupta <37578595+gilann@users.noreply.github.com>
2018-09-07 06:33:47 +0530

    Uploaded my moving circle files.

---
+++ myMovingCircle/sketch.js
@@ -0,0 +1,16 @@
+var circleX = 0;
+var circleY = 0;
+var sizeE = 5;
+function setup() {
+	createCanvas(600, 600);
+}
+
+function draw() {
+	background(200,200,200);
+	ellipse(circleX, circleY, sizeE, sizeE);
+	fill (200,0,255);
+	sizeE = (sizeE+1)%120; 
+	circleX= (circleX+5)%600;
+	circleY = (circleY+5)%600;
+
+}
